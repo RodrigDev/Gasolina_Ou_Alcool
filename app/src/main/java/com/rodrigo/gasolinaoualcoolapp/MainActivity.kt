1bb0fe05d1dc566a1149c88d8a22243187fbc1f3
Rodrigo de Oliveira <rodrigdeveloper@gmail.com>
2021-07-04 17:10:23 -0300

    Finish app

---
+++ app/src/main/java/com/rodrigo/gasolinaoualcoolapp/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.rodrigo.gasolinaoualcoolapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun calcularPreco(view: View){
+
+        //Recuperar valores digitados
+        //val precoAlcool = findViewById<EditText>(R.id.editTextAlcool)
+        //val precoGasolina = findViewById<EditText>(R.id.editTextGasolina)
+        val precoAlcool = editTextAlcool.text.toString()
+        val precoGasolina = editTextGasolina.text.toString()
+
+        val validaCampos = validarCampos(precoAlcool, precoGasolina)
+        if(validaCampos){
+            calcularMelhorPreco( precoAlcool, precoGasolina )
+        }else {
+            textResultado.text = "Preencha os preços primeiro!"
+        }
+
+    }
+
+    fun calcularMelhorPreco( precoAlcool: String, precoGasolina: String ){
+
+       //Converter valores string para numeros
+        val valorAlcool = precoAlcool.toDouble()
+        val valorGasolina = precoGasolina.toDouble()
+
+        val resultadoPreco = valorAlcool / valorGasolina
+
+        if( resultadoPreco >= 0.7 ){
+            textResultado.text = "Melhor usar Gasolina"
+        }else {
+            textResultado.text = "Melhor usar Álcool"
+        }
+
+
+    }
+
+    fun validarCampos(precoAlcool: String, precoGasolina: String) : Boolean {
+
+        var camposValidados: Boolean = true
+        if ( precoAlcool == null || precoAlcool.equals("") ){
+            camposValidados = false
+        }else if(precoGasolina == null || precoGasolina.equals("")){
+            camposValidados = false
+        }
+
+        return camposValidados
+    }
+
+}
